dbcc4cb6f05ff1af265ff7a2b9201e483ad73dfb
ibn5x <kiakilipictures@gmail>
2021-07-24 14:25:50 -0400

    implemented header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import snowman from '../../snowman.png';
+
+const Heady = styled.header`
+        background-color: #282c34;
+        min-height: 20vh;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+`;
+
+const DIVAPP = styled.div`
+     text-align: center;
+     background-color: darkgrey;
+`;
+const H1 = styled.h1`
+      font-size: 4rem;
+      color: white;
+`;
+const Logo = styled.img`
+     height: 8rem;
+     pointer-events: none;
+
+`;
+
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <DIVAPP>
+                <Heady>
+                    <Logo src={snowman} alt="logo" />
+                    <H1 className="App-title">
+                        Snowtrap DEX
+                    </H1>
+
+                </Heady>
+            </DIVAPP>
+        );
+    }
+}
